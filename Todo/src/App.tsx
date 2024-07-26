5d1a76fe91887241d9576ef9c981423be96d94d8
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-07-27 02:11:07 +0300

    New Task Add

--- Todo/src/App.tsx
+++ Todo/src/App.tsx
@@ -1,13 +1,48 @@
 import React,{FC} from 'react'
 import './App.css'
-
+import { useState } from 'react'
+import { TodoType } from './Apptypes'
 
 const App: FC = () => {
 
 
+
+
+  const [task, setTask] = useState<string>('');
+  const [workDay, setWorkDay] = useState<number>(0);
+  const [todoList , setTodoList] = useState<TodoType[]>([]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    if(event.target.name === "task")
+    {
+      setTask(event.target.value)
+    }
+    else
+    {
+      setWorkDay(parseInt(event.target.value))
+    }
+    console.log(task);
+    console.log(workDay);
+  }
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
+     <input type="text" 
+     value={task} 
+     name="task"
+     placeholder="Taskınızı Giriniz" 
+     onChange={handleChange}
+     />
+
+     <input type="number" 
+     value={workDay}
+     name="workDay"
+     placeholder="Kaç günde tamamlamalısınız"
+     onChange={handleChange}
+     />
+     <button>Yeni Task Ekle</button>
+
     </div>
   )
 }
---
+++ Todo/src/Apptypes.ts
@@ -0,0 +1,5 @@
+export type TodoType = {
+    taskName:string;
+    workDay:number;
+
+  };
