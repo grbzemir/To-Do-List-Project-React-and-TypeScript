6f27eb444115646e524293adbbecb06877b4089d
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-07-27 01:56:31 +0300

    first commit

---
+++ Todo/src/App.tsx
@@ -0,0 +1,15 @@
+import React,{FC} from 'react'
+import './App.css'
+
+
+const App: FC = () => {
+
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+    </div>
+  )
+}
+
+export default App
